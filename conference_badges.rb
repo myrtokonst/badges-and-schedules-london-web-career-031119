687fb02f541bd585630f7add7d1967e1c3e4751c
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 13:57:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(attendees)
 room_assignments = []
 attendees.each_with_index do |n,i|
-  room_assignments.push("Hello " + attendees[i]+ "! You\'ll be assigned to room #{n}!")
+  room_assignments.push("Hello, " + attendees[i]+ "! You\'ll be assigned to room #{n}!")
 end
  return room_assignments
 end
