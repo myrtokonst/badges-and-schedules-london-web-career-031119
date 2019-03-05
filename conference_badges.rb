ec61747a2c9473d20d3e1885134e5e2450872115
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 13:27:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,5 @@ end
 
 def assign_rooms(attendees)
 attendees.each do |n|
-  phrase = "Hello" attendees[n]! You\'ll be assigned to room 
+  phrase = "Hello attendees[n]! You\'ll be assigned to room #{n}!"
 end
